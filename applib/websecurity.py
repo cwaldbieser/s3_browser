a2529fd58ee2ab74c5117a32280a2db3ddc8a2f8
Carl Waldbieser <cwaldbieser@gmail.com>
2022-08-01 17:30:46 -0400

    Update CSP; make sure files are rooted in the correct place.
    Update CSP; make sure files are rooted in the correct place.
    E.g. use `url_for()` instead of hard coding javscript source paths.
    If you are not rooted under "/" but instead under something like
    "/dev/", then this helps.

--- applib/websecurity.py
+++ applib/websecurity.py
@@ -6,5 +6,24 @@ def create_content_security_policy():
         "default-src": [
             "'self'",
         ],
+        "script-src": [
+            "'self'",
+            "code.jquery.com",
+            "cdnjs.cloudflare.com",
+            "maxcdn.bootstrapcdn.com",
+            "cdn.jsdelivr.net",
+        ],
+        "style-src": [
+            "'self'",
+            "maxcdn.bootstrapcdn.com",
+            "use.fontawesome.com",
+        ],
+        "font-src": [
+            "use.fontawesome.com",
+        ],
+        "img-src": [
+            "data:",
+            "maxcdn.bootstrapcdn.com",
+        ],
     }
     return csp
