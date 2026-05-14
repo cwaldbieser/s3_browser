761948214a6bfbeaef0348c665e05a4f97c170ce
Carl Waldbieser <cwaldbieser@gmail.com>
2026-05-14 13:28:19 -0400

    [chore] update tls policy

--- applib/deploy_utils.py
+++ applib/deploy_utils.py
@@ -7,11 +7,16 @@ def on_update(zappa_cli):
     """
     if zappa_cli.domain is None:
         return
-    print("DOMAIN: `{}`".format(zappa_cli.domain))
+    print(f"DOMAIN: '{zappa_cli.domain}'")
     client = boto3.client("apigateway")
     client.update_domain_name(
         domainName=zappa_cli.domain,
         patchOperations=[
-            {"op": "replace", "path": "/securityPolicy", "value": "TLS_1_2"},
+            {
+                "op": "replace",
+                "path": "/securityPolicy",
+                "value": "SecurityPolicy_TLS13_2025_EDGE",
+            },
+            {"op": "replace", "path": "/endpointAccessMode", "value": "STRICT"},
         ],
     )
